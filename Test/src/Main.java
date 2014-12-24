6a244097d9c9fec55efc8082bfc30c1a5313ece2
MiroslavKocian <mirkocian@gmail.com>
2014-12-25 00:49:14 +0100

    prepared

---
+++ Test/src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+		displayGreetings();
+		data.display();
+
+
+	}
+	
+	private static void displayGreetings() {
+		System.out.println("HELLO HAPPY SALES PEOPLE!");
+		System.out.println("THIS APP SHOWS SALES DATA");
+		
+		System.out.println("Test 1");
+		System.out.println("Test 2");
+	}
+
+}
